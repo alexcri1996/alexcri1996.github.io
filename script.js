d4fe61012242856fa9de83037abbb6958817fe4e
doggie <dota.alexandru@gmail.com>
2021-02-14 00:30:05 +0200

    v0.1 - working prototype

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
-const el = document.querySelector("#module");
-const body = document.querySelector("body");
+const bodyEl = document.querySelector("body")
+const grassEl = document.querySelector(".alt-container .grass")
+const treesEl = document.querySelector(".alt-container .trees")
+const mountainsEl = document.querySelector(".alt-container .mountains")
+const speed = {
+  grass: 1,
+  trees: .7,
+  mountains: .5,
+}
 
-body.addEventListener("mousemove", (e) => {
-  el.style.backgroundPositionX = -e.offsetX + "px";
-  // el.style.backgroundPositionY = -e.offsetY + "px";
-});
+bodyEl.addEventListener("mousemove", (e) => {
+  grassEl.style.backgroundPositionX = -e.offsetX * speed.grass + "px"
+  treesEl.style.backgroundPositionX = -e.offsetX * speed.trees + "px"
+  mountainsEl.style.backgroundPositionX = -e.offsetX * speed.mountains + "px"
+  // el.style.backgroundPositionY = -e.offsetY + "px"
+})
