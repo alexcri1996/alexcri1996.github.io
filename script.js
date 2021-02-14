f2cf3f4b8ddb09f637020db2b49695b4085dc2c5
doggie <dota.alexandru@gmail.com>
2021-02-14 02:36:55 +0200

    adjusted speed + added y movement

--- script.js
+++ script.js
@@ -3,14 +3,19 @@ const grassEl = document.querySelector(".alt-container .grass")
 const treesEl = document.querySelector(".alt-container .trees")
 const mountainsEl = document.querySelector(".alt-container .mountains")
 const speed = {
-  grass: 1,
-  trees: .5,
+  grass: .5,
+  trees: .3,
   mountains: .2,
 }
 
 bodyEl.addEventListener("mousemove", (e) => {
-  grassEl.style.backgroundPositionX = -e.offsetX * speed.grass + "px"
-  treesEl.style.backgroundPositionX = -e.offsetX * speed.trees + "px"
-  mountainsEl.style.backgroundPositionX = -e.offsetX * speed.mountains + "px"
-  // el.style.backgroundPositionY = -e.offsetY + "px"
+  grassEl.style.backgroundPositionX = +e.offsetX * speed.grass + "px"
+  treesEl.style.backgroundPositionX = +e.offsetX * speed.trees + "px"
+  mountainsEl.style.backgroundPositionX = +e.offsetX * speed.mountains + "px"
+})
+
+bodyEl.addEventListener("mousemove", (e) => {
+  // grassEl.style.backgroundPositionY = -e.offsetY * speed.grass/75 + "px"
+  treesEl.style.backgroundPositionY = +e.offsetY * speed.trees/50 + "px"
+  mountainsEl.style.backgroundPositionY = +e.offsetY * speed.mountains/50 + "px"
 })
